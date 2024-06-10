6a950763928cd0f276ca45846066eff73ce37d34
midCenturyDude7 <mgriffes@gmail.com>
2024-06-09 21:17:44 -0700

    Completed project

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24"></div>
+    <div className="flex h-[100vh] w-[100vw] flex-col items-center justify-center bg-slate-950 text-xs text-slate-200/70">
+      <div className="card-wrapper h-[200px] w-[200px]">
+        <div className="card-content flex items-center justify-center text-xs">
+          <div className="max-w-[90%] text-center">
+            Animated Border Gradients with Tailwindcss
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
